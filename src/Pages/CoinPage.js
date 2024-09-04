5bf01460e13fbfe7556d3445f850230c76478065
sarthaktayal69 <sarthak14102002@gmail.com>
2024-09-04 09:36:28 +0530

    first commit

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -2,7 +2,7 @@ import { LinearProgress, makeStyles, Typography } from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ReactHtmlParser from "react-html-parser";
+import ReactHtmlParser from "html-react-parser";
 import CoinInfo from "../components/CoinInfo";
 import { SingleCoin } from "../config/api";
 import { numberWithCommas } from "../components/CoinsTable";
